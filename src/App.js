a8b1309404c9d8a7b12b9b6839b387f901b1daa3
melodybxz <melodybxz@gmail.com>
2025-02-08 21:27:52 -0800

    react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+function App() {
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button class="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+export default App;
